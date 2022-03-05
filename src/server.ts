8d050e97da8bf0fcb0a7cd3c1a84c09e5fe3f1cb
David Eastmond <davebuck1981@gmail.com>
2022-03-05 09:40:31 -0500

    installed body parser, cleaned up server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 require("dotenv").config();
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 import express from "express";
 import cors from "cors";
 import connectDB from "./database.config";
+
+import authenticationRouter from "./routes/authentication/authentication"
 import {
-  // checkEnvironmentVariables,
   IS_PRODUCTION,
 } from "./check-environment-variables";
+import { validateAPIKey } from "./routes/authentication/middleware/verify-api-key";
 
 const app = express();
 
-// const DOMAIN = IS_PRODUCTION
-//   ? process.env.PRODUCTION_COOKIE_DOMAIN
-//   : process.env.DEV_COOKIE_DOMAIN;
+
 
 if (IS_PRODUCTION) {
 	app.set("trust proxy", 1);
@@ -24,17 +24,21 @@ app.use(
     origin: true,
   })
 );
+app.use(bodyParser.json());
 // IS_PRODUCTION && app.use(validateAPIToken);
 
 app.set("port", process.env.PORT || 5000);
 
-app.get("/", (_req, res) => {
-  res.send("API Running");
+app.get("/", validateAPIKey, (_req, res) => {
+  res.status(200).send({ status: "API is running" });
 });
+
+app.use("/api/auth", authenticationRouter)
 const port = app.get("port");
 
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
 );
+
 connectDB();
 export default server;
