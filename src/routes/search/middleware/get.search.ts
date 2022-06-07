186d2bfe4a9912a22bdffe18210cb8b6ec2bfe6c
David Eastmond <davebuck1981@gmail.com>
2022-06-07 18:31:58 -0400

    update search route

--- src/routes/search/middleware/get.search.ts
+++ src/routes/search/middleware/get.search.ts
@@ -3,7 +3,9 @@ import { QuerySearch } from "../../../search/search-utility";
 
 export const doSearch = async (req: Request, res: Response) => {
   const { data } = req.query;
-  const searchObject = new QuerySearch(data as string);
+  const userId = res.locals.session._id;
+
+  const searchObject = new QuerySearch(data as string, userId as string);
   const results = await searchObject.getResults();
   res.status(200).send(results);
 };
