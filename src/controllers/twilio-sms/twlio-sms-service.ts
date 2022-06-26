5af3da26050fa9d9c576f5c69c34af127cb18bdb
David Eastmond <davebuck1981@gmail.com>
2022-06-26 12:56:48 -0400

    twilio functionality for 2fa

---
+++ src/controllers/twilio-sms/twlio-sms-service.ts
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+import { IS_PRODUCTION } from "../../check-environment-variables";
+import { TMessageData } from "./definitions";
+const accountSid = IS_PRODUCTION
+  ? process.env.PRODUCTION_TWILIO_ACCOUNT_SID
+  : process.env.DEV_TWILIO_ACCOUNT_SID;
+
+const authToken = IS_PRODUCTION
+  ? process.env.PRODUCTION_TWILIO_AUTH_TOKEN
+  : process.env.DEV_TWILIO_AUTH_TOKEN;
+
+const sourceCTN = IS_PRODUCTION
+  ? process.env.PRODUCTION_CTN
+  : process.env.DEV_CTN;
+const client = require("twilio")(accountSid, authToken);
+
+export async function createTwilioTextMessage(messageData: TMessageData) {
+  try {
+    const message = await client.messages.create({
+      ...messageData,
+      from: sourceCTN,
+    });
+    console.log(message);
+  } catch (exception: any) {
+    console.log(exception.message);
+  }
+}
--- src/models/user/user.schema.ts
+++ src/models/user/user.schema.ts
@@ -28,6 +28,14 @@ const userSchema = new Schema<IUser>(
         ],
         default: [],
       },
+      twoFactorAuthentication: {
+        enabled: { type: Boolean, default: false },
+        userCtn: { type: String, default: null },
+        userCtnVerified: { type: Boolean, default: false },
+        token: { type: String, default: null },
+        tokenCreated: { type: Date, default: null },
+        tokenExpires: { type: Date, default: null },
+      },
     },
   },
   {
