93953796c8c69ae6676decb6899bebb4c55902fb
David Eastmond <davebuck1981@gmail.com>
2022-07-06 19:38:32 -0400

    refactor get.user.security

--- src/routes/user/middleware/get.user.security.ts
+++ src/routes/user/middleware/get.user.security.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { UserModel } from "../../../models/user/user.schema";
 import { IUserDocument } from "../../../models/user/user.types";
+
 export const returnUsersSecurityQuestionsIfAny = async (
   req: Request,
   res: Response
@@ -31,6 +32,23 @@ export const returnUsersSecurityQuestionsIfAny = async (
     const defaultSecurityQuestionData =
       await createEmptySecurityQuestionPropertyOnUserDocument(user);
     return res.status(201).send(defaultSecurityQuestionData);
+  } catch (exception: any) {
+    return res.status(500).send({ error: exception.message });
+  }
+};
+
+export const getTwoFactorAuthStatus = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await UserModel.findById(userId);
+    if (user) {
+      return res.status(200).send({
+        twoFactorAuthentication: {
+          enabled: user.security.twoFactorAuthentication.enabled || false,
+        },
+      });
+    }
   } catch (exception: any) {
     return res.status(500).send({ error: exception.message });
   }
