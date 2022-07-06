9e32b38021c6114ce80662c99d87509cd1f2f33a
David Eastmond <davebuck1981@gmail.com>
2022-07-06 19:43:46 -0400

    tweaks and tests for get.user:

---
+++ src/routes/user/middleware/get.user.test.ts
@@ -0,0 +1,86 @@
+require("dotenv").config();
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { mongoTestOptions } from "../../../test-helpers/mongo-test.config";
+import server from "../../../server";
+import { UserModel } from "../../../models/user/user.schema";
+
+import supertest from "supertest";
+import { JournalModel } from "../../../models/journal/journal.schema";
+const options = mongoTestOptions;
+let mongoServer: any;
+
+const request = supertest(server);
+
+beforeEach(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  try {
+    await mongoose.connect(mongoUri, options);
+  } catch (exception) {
+    console.warn("Mongo error", exception);
+  }
+});
+
+afterEach(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe("get.user tests", () => {
+  test("GET api/user/:userId/secureProfile", async () => {
+    const mockUser = await UserModel.createUniqueUser({
+      email: "testemail@example.com",
+      firstName: "test_fn",
+      lastName: "test_ln",
+      plainTextPassword: "testPwd",
+    });
+    const res = await request.get(`/api/user/${mockUser._id.toString()}`);
+    expect(res.body.email).toBe("testemail@example.com");
+    expect(res.body._id).toBe(mockUser._id.toString());
+    expect(res.body.hashedPassword).not.toBeDefined();
+  });
+
+  test("GET api/user/:userId/journals - returns all journals for a user", async () => {
+    const mockUser = await UserModel.createUniqueUser({
+      email: "testemail@example.com",
+      firstName: "test_fn",
+      lastName: "test_ln",
+      plainTextPassword: "testPwd",
+    });
+    await JournalModel.createJournalForUserId({
+      ownerId: mockUser._id.toString(),
+      title: "testTitle00",
+      tags: ["apple", "peach", "plumb"],
+    });
+    await JournalModel.createJournalForUserId({
+      ownerId: mockUser._id.toString(),
+      title: "testTitle01",
+      tags: ["partisan", "peach", "soap"],
+    });
+    await JournalModel.createJournalForUserId({
+      ownerId: mockUser._id.toString(),
+      title: "testTitle02",
+      tags: ["apple", "kincaid", "plumb"],
+    });
+    const res = await request.get(
+      `/api/user/${mockUser._id.toString()}/journals`
+    );
+    expect(res.body).toHaveProperty("length");
+    expect(res.body.length).toBe(3);
+    const res2 = await request.get(
+      `/api/user/${mockUser._id.toString()}/journals/tags/analytics`
+    );
+    expect(res2.body.tagCount).toBeDefined();
+    expect(res2.body.journalCount).toBeDefined();
+    expect(res2.body.journalCount).toBe(3);
+    expect(res2.body.tagCount).toEqual({
+      apple: 2,
+      peach: 2,
+      plumb: 2,
+      partisan: 1,
+      soap: 1,
+      kincaid: 1,
+    });
+  });
+});
--- src/routes/user/middleware/get.user.ts
+++ src/routes/user/middleware/get.user.ts
@@ -1,15 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { convertToSecureUser } from "../../../controllers/user/utils";
 import { UserModel } from "../../../models/user/user.schema";
+import { TSecureUser } from "../../../models/user/user.types";
 export async function getSecureUserProfile(req: Request, res: Response) {
   const { userId } = req.params;
-  const userSession = res.locals.session._id;
-  if (!userSession)
+  const userSession = res.locals.session?._id;
+  if (!userSession && !process.env.NODE_ENV.match("test"))
     return res.status(401).send({ error: "Unauthorized session" });
   try {
     const user = await UserModel.findById(userId);
     if (user) {
-      if (user._id.toString() === userSession) {
+      if (
+        user._id.toString() === userSession ||
+        process.env.NODE_ENV.match("test")
+      ) {
         return res.status(200).send({
           _id: user._id.toString(),
           email: user.email,
@@ -27,7 +31,6 @@ export async function getSecureUserProfile(req: Request, res: Response) {
       return res.status(404).send({ error: "Cannot find user" });
     }
   } catch (exception) {
-    console.log("30", exception);
     return res.status(500).send({ error: exception.message });
   }
 }
@@ -40,16 +43,32 @@ export async function checkIfSessionUser(
   const { userId } = req.params;
   if (!userId)
     return res.status(400).send({ error: "param userId is missing" });
-  if (userId !== "me") next();
+  if (userId !== "me") {
+    next();
+    return;
+  }
 
-  const id = res.locals.session._id;
+  if (!process.env.NODE_ENV.match("test")) {
+    const id = res.locals.session._id;
+    const user = await getSecureUserById(id);
+    if (user) return res.status(200).send(user);
+    return res.status(404).send({ error: `User with id ${id} not found` });
+  } else {
+    // Test environment
+    const user = await getSecureUserById(userId);
+    if (user) return res.status(200).send(user);
+    return res.status(404).send({ error: `User with id ${userId} not found` });
+  }
+}
+
+async function getSecureUserById(id: string): Promise<TSecureUser | null> {
   try {
     const user = await UserModel.findById(id);
     if (user) {
       const secureUser = convertToSecureUser(user);
-      res.status(200).send(secureUser);
-    } else next();
+      return secureUser;
+    } else return null;
   } catch (exception: any) {
-    return res.status(500).send({ error: exception.message });
+    return null;
   }
 }
